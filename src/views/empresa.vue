dbc05197eb8d729db290388122bb51529f720bc7
jesus narvaez <jesussfrannardi777@gmail.com>
2020-11-12 12:10:54 -0500

    cargar empresa

--- src/views/empresa.vue
+++ src/views/empresa.vue
@@ -1,131 +1,133 @@
 <template>
- 
-  <container class="container" >
-    <div class="con text-center m-4 p-4">
+<container class="container">
+  <div class="con text-center m-4 p-4">
 
-      <h1>Iniciar sesion</h1>
+    <h1>Registro de la empresa</h1>
 
-      <form @submit.prevent="guardar">
-        <div class="row">
-             <div class="col col-lg-4 col-xl-3">
-              <label for="nombre">Tipo de establecimiento</label> <br>
-              <input type="text" placeholder="Nombre completo..." v-model="nombre" id="nombre"> 
-             </div>
-            <div class="col col-lg-4 col-xl-3">
-              <label for="correo">Email</label> <br>
-              <input type="text" placeholder="Correo..." v-model="correo" id="correo">
-            </div>
-            <div class="col col-lg-4 col-xl-3">
-              <label for="clave">Password</label> <br>
-              <input type="password" placeholder="Contraseña..." v-model="clave" id="clave"> 
-            </div>
-            <div class="col col-lg-4 col-xl-3">
-              <label for="telefono">Telefono</label> <br>
-              <input type="text" placeholder="celular..." v-model="telefono" id="telefono"> 
-            </div>
-            <div class="col col-lg-4 col-xl-3">
-              <label for="direccion">direccion</label> <br>
-              <input type="text" placeholder="Direccion residencia..." v-model="direccion" id="direccion"> 
-            </div>
-            <div class="col col-lg-4 col-xl-3">
-              <label for="empresa">Nombre Empresa</label> <br>
-              <input type="text" placeholder="Nombre E..." v-model="empresa" id="empresa"> 
-            </div>
-   
+    <form @submit.prevent="guardar" enctype="multipart/form-data">
+      <div class="row">
+        <div class="col col-lg-4 col-xl-3">
+          <label for="nombre">Nombre de la empresa</label> <br>
+          <input type="text" placeholder="Nombre completo..." v-model="nombre" name="nombre">
+        </div>
+        <div class="col col-lg-4 col-xl-3">
+          <label for="correo">Email</label> <br>
+          <input type="text" placeholder="Correo..." v-model="correo" name="correo">
+        </div>
+        <div class="col col-lg-4 col-xl-3">
+          <label for="telefono">Telefono</label> <br>
+          <input type="text" placeholder="celular..." v-model="telefono" name="telefono">
+        </div>
+        <div class="col col-lg-4 col-xl-3">
+          <label for="clave">Password</label> <br>
+          <input type="password" placeholder="Contraseña..." v-model="clave" name="clave">
+        </div>
 
+        <div class="col col-lg-4 col-xl-3">
+          <label for="direccion">direccion</label> <br>
+          <input type="text" placeholder="Direccion residencia..." v-model="direccion" name="direccion">
+        </div>
 
-        </div> <br>
-              <div class="d-flex justify-content-center">
-                <div class="b2 row ">
-                  <input type="file" ref="file" id="imagen" @change="cambiarArchivo" placeholder="" accept="image/jpeg, image/png, image/jpg, image/gif" >
-                </div>
-              </div>
-               
-          
+      </div> <br>
+      <div class="d-flex justify-content-center">
+        <div class="b2 row ">
+          <input type="file" ref="file" id="imagen" placeholder="" accept="image/*">
+        </div>
+      </div>
+      <div class="col-12 col-md-6 text-center justify-content-center mx-auto" style="height: 500px">
+        <p><b>Previsualizacion:</b></p>
+        <br />
+        <div v-if="image === ''"></div>
+        <div>
+          <img :src="image" alt="" height="400px" width="400px" />
+        </div>
+      </div>
 
-           <div>   
-          <div  v-if="error" class="nota p-2 m-2 col-lg-6 aling-center" >
-            <samp class="">las credenciales no son coreectas</samp>
-          </div> 
-         
-            
-          </div>
-          
-            <button type="submit" class="row bg-dark text-white m-4">Registrar</button>
-      </form>
-    </div>
-  </container>
-    
-
+      <div>
+        <div v-if="error" class="nota p-2 m-2 col-lg-6 aling-center">
+          <samp class="">las credenciales no son coreectas</samp>
+        </div>
+      </div>
+      <button type="submit" class="row bg-dark text-white m-4">Registrar</button>
+    </form>
+  </div>
+</container>
 </template>
 
 <script>
-import axios from 'axios'; 
+import axios from 'axios';
 
 export default {
   name: 'empresa',
-    data(){
-      return{
-        nombre: '',
-        correo: '',
-        clave: '',
-        telefono: '',
-        direccion: '',
-        empresa: '',
-        imagen: null,
+  data() {
+    return {
+      nombre: '',
+      correo: '',
+      clave: '',
+      verimagen: '',
+      telefono: '',
+      direccion: '',
+      empresa: '',
+      image: '',
+      imagen: null,
 
-        error: false,
-      }
-    },
-    
-    methods: {
+      error: false,
+    }
+  },
 
-    cambiarArchivo(e){
-        this.imagen = e.target.files[0]
-        console.log(e.target.files[0])
-    },
+  methods: {
 
-    guardar(){
+    /* cambiarArchivo(e) {
+      this.imagen = e.target.files[0]
+      console.log(e.target.files[0])
+    }, */
+    /*    prevista(e) {
+         this.image = URL.createObjectURL(e.target.files[0]);
+         this.verimagen = e.target.files[0];
+       }, */
 
+    guardar() {
+      const tokens = localStorage.getItem("token");
       let form = new FormData()
-
+      form.append("files.imagen", this.verimagen);
       form.append('data', JSON.stringify({
-          "nombre": this.nombre,
-          "correo":this.correo,
-          "clave":this.clave,
-          "telefono":this.telefono,
-          "direccion":this.direccion,
-          "nempresa":this.empresa,
-          
-      })),
-      form.append('files.img', this.imagen)
-      
+          nombre: this.nombre,
+          correo: this.correo,
+          clave: this.clave,
+          telefono: this.telefono,
+          direccion: this.direccion
+        })),
+        /* form.append('files.img', this.imagen) */
 
-      axios.post("http://localhost:1337/empresas",form
-             ).then((response) => {
-                console.log('se creo un usuario publico')
-             });  
-      
+        axios.post("http://localhost:1337/empresas", form, {
+          headers: {
+            Authorization: "Bearer " + tokens,
+            "Content-Type": "multipart/form-data",
 
-      axios.post("http://localhost:1337/auth/local/register",{
-               "username": this.nombre,
-	           "email": this.correo,
-	           "password": this.clave
-        }
-        ).then((response) => {  
-                this.$router.push('/')
-        });  
-        },
+          },
 
+        }).then((response) => {
+          console.log('se creo un usuario publico')
+        }).catch((err) => {
+          console.log("FALLO")
+        });
+
+      /*  axios.post("http://localhost:1337/auth/local/register", {
+         "username": this.nombre,
+         "email": this.correo,
+         "password": this.clave
+       }).then((response) => {
+         this.$router.push('/')
+       }); */
     },
-  
-}
 
+  },
+
+}
 </script>
 
 <style>
-
-.con{
+.con {
   background: rgb(221, 218, 218);
   border-block: 10px;
   border-block-color: black;
@@ -133,32 +135,35 @@ export default {
 
 }
 
-input{
+input {
   border-radius: 7px;
-  border-block-end: lightseagreen ;
+  border-block-end: lightseagreen;
   transition: inherit;
-  
+
 }
-button{
+
+button {
   border-top: khaki;
   border-radius: 10px;
 }
-.nota{
+
+.nota {
   border-radius: 30px;
   height: 200px;
   background: rgb(126, 200, 230);
-  
+
 }
 
-.b2{
+.b2 {
   background-size: 300px;
   height: 150px;
-  background:url('img/fond.png');
+  background: url('img/fond.png');
   border-radius: 10px;
   text-align: center;
 }
-input#imagen{
-  justify-content: center; 
+
+input#imagen {
+  justify-content: center;
   width: 300px;
   height: 300px;
   opacity: 0;
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,66 +1,59 @@
 <template>
-   <container class="container" >
-    <div class="con text-center m-4 p-4">
+<container class="container">
+  <div class="con text-center m-4 p-4">
+    <h1>Registrar</h1>
 
-      <h1>Registrar </h1>
+    <form @submit="guardar">
+      <div class="row">
+        <div class="col col-lg-4 col-xl-3">
+          <label for="nombre">Nombre</label> <br />
+          <input type="text" placeholder="Nombre completo..." v-model="nombre" id="nombre" />
+        </div>
+        <div class="col col-lg-4 col-xl-3">
+          <label for="correo">Email</label> <br />
+          <input type="text" placeholder="Correo..." v-model="gmail" id="gmail" />
+        </div>
+        <div class="col col-lg-4 col-xl-3">
+          <label for="clave">Password</label> <br />
+          <input type="password" placeholder="Contraseña..." v-model="password" id="password" />
+        </div>
+      </div>
 
-      <form @submit="guardar">
-        <div class="row">
-             <div class="col col-lg-4 col-xl-3">
-              <label for="nombre">Nombre</label> <br>
-              <input type="text" placeholder="Nombre completo..." v-model="nombre" id="nombre"> 
-             </div>
-            <div class="col col-lg-4 col-xl-3">
-              <label for="correo">Email</label> <br>
-              <input type="text" placeholder="Correo..." v-model="gmail" id="gmail">
-            </div>
-            <div class="col col-lg-4 col-xl-3">
-              <label for="clave">Password</label> <br>
-              <input type="password" placeholder="Contraseña..." v-model="password" id="password"> 
-            </div>
-        </div> 
-          
+      <div>
+        <div v-if="error" class="nota p-2 m-2 col-lg-6 aling-center">
+          <samp class="">las credenciales no son coreectas</samp>
+        </div>
+      </div>
 
-           <div>   
-          <div  v-if="error" class="nota p-2 m-2 col-lg-6 aling-center" >
-            <samp class="">las credenciales no son coreectas</samp>
-          </div> 
-         
-            
-          </div>
-          
-            <button type="submit" class="row bg-dark text-white m-4">Registrar</button>
-      </form>
-    </div>
-  </container>
+      <button type="submit" class="row bg-dark text-white m-4">Registrar</button>
+    </form>
+  </div>
+</container>
 </template>
 
 <script>
 export default {
-    name: "login",
-    data(){
-      return{
-          nombre:"",
-          gmail:"",
-          password:""
-      }
-    }, 
+  name: "login",
+  data() {
+    return {
+      nombre: "",
+      gmail: "",
+      password: "",
+    };
+  },
 
-methods: {
-    guardar(){
-
-      axios.post("http://localhost:1337/auth/local/register",{
-               "username": this.nombre,
-	           "email": this.gmail,
-	           "password": this.password
-        }
-        ).then((response) => {  
-                this.$router.push('/')
-        });  
-        },
-
+  methods: {
+    guardar() {
+      axios
+        .post("http://localhost:1337/auth/local/register", {
+          username: this.nombre,
+          email: this.gmail,
+          password: this.password,
+        })
+        .then((response) => {
+          this.$router.push("/");
+        });
     },
-
-}
-
+  },
+};
 </script>
